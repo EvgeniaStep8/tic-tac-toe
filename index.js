173d230d4d4f36765c3f2705db5966bace49bfe2
Evgenia Stepanova <evgeniast85es@yandex.ru>
2023-09-11 22:37:24 +0300

    add new raund and players name

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const game = document.querySelector(".game");
 const cells = document.querySelectorAll(".cell");
+const endOverlay = document.querySelector(".end-overlay");
 const winner = document.querySelector(".winner");
 const raund = document.querySelector(".raund");
-const newGameButton = document.querySelector(".new-game");
+const players = document.querySelector(".players");
+const newRaundButton = document.querySelector(".new-raund");
+const popup = document.querySelector(".popup");
+const form = document.forms.players;
+const x = document.querySelector("#x");
+const o = document.querySelector("#o");
 
 let gameRaund = 1;
 let move = 1;
 const lastAvailableMove = 9;
+let player1, player2;
 
 const winnerCombinations = [
   [0, 1, 2],
@@ -16,8 +23,20 @@ const winnerCombinations = [
   [1, 4, 7],
   [2, 5, 8],
   [0, 4, 8],
-  [2, 4, 6]
-]
+  [2, 4, 6],
+];
+
+const handleFormSubmit = (evt) => {
+  evt.preventDefault();
+  player1 = form.player1.value;
+  player2 = form.player2.value;
+
+  players.textContent = `${player1} vs ${player2}`;
+  x.textContent = `Крестики: ${player1}`;
+  o.textContent = `Нолики: ${player2}`;
+
+  popup.classList.add("popup-close");
+};
 
 const resetField = () => {
   cells.forEach((cell) => {
@@ -25,40 +44,45 @@ const resetField = () => {
     if (cell.classList.contains("cell-winner")) {
       cell.classList.remove("cell-winner");
     }
-  })
-}
+  });
+};
 
 const newGame = () => {
   resetField();
   gameRaund++;
   move = 1;
   raund.textContent = `Раунд ${gameRaund}`;
-  winner.textContent = `Победитель:`;
-}
+  endOverlay.classList.remove("end-overlay-visible");
+};
 
 const endOfGame = (player) => {
   if (player === "draw") {
     winner.textContent = "Ничья";
     return;
   }
-  const win = player === "X" ? "крестики" : "нолики";
+  const win = player === "X" ? player1 : player2;
   winner.textContent = `Победитель: ${win}`;
-}
+  endOverlay.classList.add("end-overlay-visible");
+};
 
 const checkWinner = (player) => {
   let winnerCombination, gameCombination;
   for (let i = 0; i < winnerCombinations.length; i++) {
     winnerCombination = winnerCombinations[i];
-    gameCombination = [ cells[winnerCombination[0]],  cells[winnerCombination[1]], cells[winnerCombination[2]]];
+    gameCombination = [
+      cells[winnerCombination[0]],
+      cells[winnerCombination[1]],
+      cells[winnerCombination[2]],
+    ];
     if (gameCombination.every((cell) => cell.textContent === player)) {
       gameCombination.forEach((cell) => {
         cell.classList.add("cell-winner");
-      })
+      });
       return true;
     }
   }
   return false;
-}
+};
 
 const handleGameClick = (evt) => {
   if (evt.target !== evt.currentTarget && evt.target.textContent === "") {
@@ -73,7 +97,8 @@ const handleGameClick = (evt) => {
     }
     move++;
   }
-}
+};
 
+form.addEventListener("submit", handleFormSubmit);
 game.addEventListener("click", handleGameClick);
-newGameButton.addEventListener("click", newGame);
+newRaundButton.addEventListener("click", newGame);
