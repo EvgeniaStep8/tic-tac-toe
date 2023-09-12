a67ebf8ba8d1d68935db17cf410e84112421a6c9
Evgenia Stepanova <evgeniast85es@gmail.com>
2023-09-12 12:06:30 +0300

    add new game

--- index.js
+++ index.js
@@ -5,15 +5,18 @@ const winner = document.querySelector(".winner");
 const raund = document.querySelector(".raund");
 const players = document.querySelector(".players");
 const newRaundButton = document.querySelector(".new-raund");
+const newGameButton = document.querySelector(".new-game");
 const popup = document.querySelector(".popup");
 const form = document.forms.players;
 const x = document.querySelector("#x");
 const o = document.querySelector("#o");
+const count = document.querySelector(".count");
 
 let gameRaund = 1;
 let move = 1;
 const lastAvailableMove = 9;
-let player1, player2;
+let player1 = { count: 0, symbol: "X" };
+let player2 = { count: 0, symbol: "O"};
 
 const winnerCombinations = [
   [0, 1, 2],
@@ -26,16 +29,31 @@ const winnerCombinations = [
   [2, 4, 6],
 ];
 
+const openPopup = () => {
+  popup.classList.add("popup-visible");
+}
+
+const closePopup = () => {
+  popup.classList.remove("popup-visible");
+}
+
+const resetInputs = (form) => {
+  form.querySelectorAll(".input").forEach((input) => {
+    input.value = "";
+  });
+}
+
 const handleFormSubmit = (evt) => {
   evt.preventDefault();
-  player1 = form.player1.value;
-  player2 = form.player2.value;
+  player1.name = form.player1.value;
+  player2.name = form.player2.value;
 
-  players.textContent = `${player1} vs ${player2}`;
-  x.textContent = `Крестики: ${player1}`;
-  o.textContent = `Нолики: ${player2}`;
+  players.textContent = `${player1.name} vs ${player2.name}`;
+  x.textContent = `Крестики: ${player1.name}`;
+  o.textContent = `Нолики: ${player2.name}`;
 
-  popup.classList.add("popup-close");
+ resetInputs(form);
+ closePopup();
 };
 
 const resetField = () => {
@@ -49,20 +67,56 @@ const resetField = () => {
 
 const newGame = () => {
   resetField();
+  gameRaund = 1;
+  move = 1;
+
+  player1 = { count: 0, symbol: "X" };
+  player2 = { count: 0, symbol: "O"};
+
+  x.textContent = "";
+  o.textContent = "";
+  count.textContent = `Счёт: ${player1.count} : ${player2.count}`;
+  raund.textContent = `Раунд ${gameRaund}`;
+  endOverlay.classList.remove("end-overlay-visible");
+
+  openPopup();
+}
+
+const newRaund = () => {
+  resetField();
   gameRaund++;
   move = 1;
   raund.textContent = `Раунд ${gameRaund}`;
   endOverlay.classList.remove("end-overlay-visible");
+  
+  if (gameRaund % 2 === 0) {
+    player1.symbol = "O";
+    player2.symbol = "X";
+    x.textContent = `Крестики: ${player2.name}`;
+    o.textContent = `Нолики: ${player1.name}`;
+  } else {
+    player1.symbol = "X";
+    player2.symbol = "O";
+    x.textContent = `Крестики: ${player1.name}`;
+    o.textContent = `Нолики: ${player2.name}`;
+  }
+  
 };
 
 const endOfGame = (player) => {
   if (player === "draw") {
     winner.textContent = "Ничья";
-    return;
+    player1.count++;
+    player2.count++;
+  } else {
+    const win = player1.symbol === player ? player1 : player2;
+    win.count++;
+
+    winner.textContent = `Победитель: ${win.name}`;
   }
-  const win = player === "X" ? player1 : player2;
-  winner.textContent = `Победитель: ${win}`;
+
   endOverlay.classList.add("end-overlay-visible");
+  count.textContent = `Счёт: ${player1.count} : ${player2.count}`;
 };
 
 const checkWinner = (player) => {
@@ -101,4 +155,5 @@ const handleGameClick = (evt) => {
 
 form.addEventListener("submit", handleFormSubmit);
 game.addEventListener("click", handleGameClick);
-newRaundButton.addEventListener("click", newGame);
+newRaundButton.addEventListener("click", newRaund);
+newGameButton.addEventListener("click", newGame);
